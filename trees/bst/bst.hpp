5611f46c3f7a4b8bfcd0ccae859bb932d459a6a0
Namanh Tran <NamanhDTran@gmail.com>
2018-08-20 16:28:49 -0700

    continued to implement bst

--- trees/bst/bst.hpp
+++ trees/bst/bst.hpp
@@ -1,22 +1,66 @@
 #include "node.hpp"
 
-template <typename E>
 class BST
 {
     public:
-        bool search();
-        void display();
+        bool search(int n);
         void removeNode();
-        void insertNode(E n);
+        void insertNode(int n);
+        void display();
 
     private:
         int size;
+        Node* root;
 };
 
-template <typename E>
-BST<E>::BST
+BST::BST()
 {
     size = 0;
+    Node* root = NULL;
 }
 
-template <typename E>
+bool BST::search(int n)
+{
+    if(size == 0)
+    {
+        cout << "The tree is empty\n";
+        return false;
+    }
+    
+    else
+    {
+        Node* temp = root;
+        while(temp != NULL)
+        {
+            if(n == data)
+            {
+                return true;
+            }
+
+            else if(n > data)
+            {
+                temp = temp->right;
+            }
+
+            else
+            {
+                temp = temp->left;
+            }
+        }
+
+        return false;
+    }
+}
+
+void BST::removeNode(int n)
+{
+    Node* temp = root;
+    Node* prev = root;
+    while(temp != NULL)
+    {
+        if(temp->data == n && temp != root)
+        {
+            
+        }
+    }
+}
