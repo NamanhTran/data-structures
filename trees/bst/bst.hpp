e62b7e665bfa960e5c0914f1c82984c872e3c435
Namanh Tran <NamanhDTran@gmail.com>
2018-08-22 13:14:41 -0700

    added more bst functions

--- trees/bst/bst.hpp
+++ trees/bst/bst.hpp
@@ -1,4 +1,5 @@
 #include "node.hpp"
+#include <queue>
 
 class BST
 {
@@ -6,17 +7,22 @@ class BST
         bool search(int n);
         void removeNode();
         void insertNode(int n);
-        void display();
-
+        int min();
+        int max();
+        int height();
+        void levelOrderDisplay();
+        void preOrderDisplay();
+        void inOrderDisplay();
+        void postOrderDisplay();
     private:
         int size;
-        Node* root;
+        Node<int>* root;
 };
 
 BST::BST()
 {
     size = 0;
-    Node* root = NULL;
+    root = NULL;
 }
 
 bool BST::search(int n)
@@ -29,7 +35,7 @@ bool BST::search(int n)
     
     else
     {
-        Node* temp = root;
+        Node<int>* temp = root;
         while(temp != NULL)
         {
             if(n == data)
@@ -52,15 +58,112 @@ bool BST::search(int n)
     }
 }
 
+//not finished
 void BST::removeNode(int n)
 {
-    Node* temp = root;
-    Node* prev = root;
+    Node<int>* temp = root;
+    Node<int>* prev = root;
     while(temp != NULL)
     {
         if(temp->data == n && temp != root)
         {
             
         }
+    }
+}
+
+void BST::insertNode(int n)
+{
+    if(root == NULL)
+    {
+        root = new Node<int>;
+        root->data = n;
+    }
+
+    else
+    {
+        Node<int> trav = root;
+        while(trav != NULL)
+        {
+            if(trav->data == n)
+            {
+                std::cout << "The data already exists in the tree" << std::endl;
+                return;
+            }
+
+            else if(trav->data > n)
+            {
+                if(trav->left == NULL)
+                {
+                    Node<int> temp = new Node;
+                    temp->data = n;
+                    trav->left = temp;
+                }
+
+                else
+                {
+                    trav = trav->left;
+                }
+            }
+
+            else
+            {
+                if(trav->right == NULL)
+                {
+                    Node<int>* temp = new Node;
+                    temp->data = n;
+                    trav->right = temp;
+                }
+
+                else
+                {
+                    trav = trav->right;
+                }
+            }
+        }
+    }
+}
+
+int BST::min()
+{
+    Node<int> trav = root;
+    while(trav->left != NULL)
+    {
+        trav = trav->left;
+    }
+
+    return trav->data;
+}
+
+int BST::max()
+{
+    Node<int> trav = root;
+    
+    while(trav->right != NULL)
+    {
+        trav = trav->right;
+    }
+
+    return trav->data;
+}
+
+int BST::findHeight(Node<int>* node)
+{
+    if(node == NULL)
+    {
+        return -1;
+    }
+
+    return max(findHeight(node->left), findHeight(node->right)) + 1;
+}
+
+void BST::levelOrderDisplay()
+{
+    queue<Node*> q;
+    q.push(root);
+    
+    while(!q.empty())
+    {
+
     }
 }
