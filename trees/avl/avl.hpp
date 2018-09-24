329328f45138b910b0bdcf6bf5237268c05b224b
Namanh <NamanhDTran@gmail.com>
2018-09-24 13:25:18 -0700

    Created updateHeight, checkRotation, and getBalanceFactor functions

--- trees/avl/avl.hpp
+++ trees/avl/avl.hpp
@@ -91,6 +91,7 @@ Node<T>* AVL<T>::insert(Node<T>* root,T value)
 
     trav = node;
 
+    // updates height of the tree and checks if rotation is needed
     for(int i = 0; i < index; i++)
     {
         checkRotations(parents[index - i - 1]);
@@ -102,6 +103,71 @@ Node<T>* AVL<T>::insert(Node<T>* root,T value)
 template <typename T>
 void AVL<T>::checkRotations(Node<T>* node)
 {
+	int bf = getBalanceFactor(node);
 
+	if(bf >= 2)
+	{
+		int leftBf = getBalanceFactor(node->left);
+
+		if(leftBf > 0)
+				leftRotation(node);
+
+		else
+				rightLeftRotation(node);
+	}
+
+	else if(bf <= -2)
+	{
+	 	int right_bf = getBalanceFactor(node->right);
+
+		if(bf > 0)
+				rightRotation(node);
+
+		else
+				leftRightRotation(node);
+	}
+
+	else
+	{
+		updateHeight(node);
+	}
+}
+
+template<typename T>
+int getBalanceFactor(Node<T>* node)
+{
+	long leftHeight = 0;
+
+	if(node->left != NULL)
+		leftHeight = node->left->height + 1;
+
+	long rightHeight = 0;
+
+	if(node->right != NULL)
+			rightHeight = node->right->height + 1;
+
+	return rightHeight - leftHeight;
+}
+
+template <typename T>
+void AVL<T>::updateHeight(Node<T>* node)
+{
+	int leftHeight = 0;
+
+	if(node->left != NULL)
+		leftHeight = node->left->height + 1
+
+	int rightHeight = 0;
+
+	if(node->right != NULL)
+		rightHeight = node->right->height + 1;
+
+	int nodeHeight = 0;
+
+	if(leftHeight > rightHeight)
+		node->height = leftHeight;
+	
+	else
+		node->height = rightHeight;	
 }
 #endif
